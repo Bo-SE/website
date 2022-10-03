dbf02130eae5650e23c474d7851b02e78e185ffe
Degra <filippodegrandi02@gmail.com>
2022-10-03 22:08:41 +0200

    Improved modules tree and files division

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
 #[macro_use] extern crate rocket;
 use rocket::{Build, Rocket};
 use rocket_dyn_templates::Template;
-use rocket::serde::Serialize;
 
-mod get_functions;
+mod routes; // using all routes from the "routes" directory
 
 #[launch]
 fn rocket() -> Rocket<Build>{
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![ //list of routes
+            routes::index::homepage,
+            // all the others
+        ])
         .attach(Template::fairing())
 }
 
-#[derive(Serialize)]
-struct Person {
-    name: String,
-    surname: String
-}
-
-#[get("/")]
-fn index() -> Template {
-    let content = Person { name: "Borino".to_string(), surname: "Stock Exchange".to_string() };
-    Template::render("index", &content)
-}
---
+++ src/routes/index.rs
@@ -0,0 +1,14 @@
+use rocket::serde::Serialize;
+use rocket_dyn_templates::Template;
+
+#[derive(Serialize)]
+struct Person {
+    name: String,
+    surname: String
+}
+
+#[get("/")]
+pub fn homepage() -> Template {
+    let content = Person { name: "Borino".to_string(), surname: "Stock Exchange".to_string() };
+    Template::render("index", &content)
+}
