30c1e482e6518bb563e839a2a8de22dbb1519af1
Lyreplus <lyreplus@gmail.com>
2022-10-05 11:37:42 +0200

    modified contact info, linked bus vector to index.html.hbs

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -24,41 +24,43 @@ pub fn homepage() -> Template {
     let busses: Busses =  crate::routes::trentino_api::parse_json("").unwrap();
 
     let context = Person { name: "Borino".to_string(), surname: "Stock Exchange".to_string() };
-    let info_banner = (
-            Bus{
-                bus_stop: "PVVLN",
-                bus_info: "5 min ritardo",
-                is_late: true
-            },
-            Bus{
-                bus_stop: "PVSL",
-                bus_info: "In orario",
-                is_late: false
-            },
-            Bus{
-                bus_stop: "PVFCSCNZ",
-                bus_info: "10 min ritardo",
-                is_late: true
-            },
-            Bus{
-                bus_stop: "PVPZMNC",
-                bus_info: "5 min ritardo",
-                is_late: true
-            },
-            Bus{
-                bus_stop: "PVPNT",
-                bus_info: "In orario",
-                is_late: false
-            },
-            Bus{
-                bus_stop: "STZNFS",
-                bus_info: "2 minuti ritardo",
-                is_late: true
-            }
-        );
 
-    Template::render("index", context! {
-        index: info_banner
-    })
+    // let info_banner = (
+    //         Bus{
+    //             bus_stop: "PVVLN",
+    //             bus_info: "5 min ritardo",
+    //             is_late: true
+    //         },
+    //         Bus{
+    //             bus_stop: "PVSL",
+    //             bus_info: "In orario",
+    //             is_late: false
+    //         },
+    //         Bus{
+    //             bus_stop: "PVFCSCNZ",
+    //             bus_info: "10 min ritardo",
+    //             is_late: true
+    //         },
+    //         Bus{
+    //             bus_stop: "PVPZMNC",
+    //             bus_info: "5 min ritardo",
+    //             is_late: true
+    //         },
+    //         Bus{
+    //             bus_stop: "PVPNT",
+    //             bus_info: "In orario",
+    //             is_late: false
+    //         },
+    //         Bus{
+    //             bus_stop: "STZNFS",
+    //             bus_info: "2 minuti ritardo",
+    //             is_late: true
+    //         }
+    //     );
+
+    let template = Template::render("index", context! {
+        index: busses
+    });
+    template
 }
 
